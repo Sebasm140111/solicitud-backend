da5391c599d61d2de7e4b60d8a666159974bbcd2
Sebasm140111 <sebasm1401@hotmail.com>
2025-06-20 21:11:57 -0500

    backup1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 import base64
 
 app = Flask(__name__)
-cloudconvert.configure(api_key=os.environ.get('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOThiYzBhYjg4ODkwZWI5ODczZDNjYzcwNjU3NjgwYjlkZjc4MjlhY2Y2NWYzMGNiZjI5OGY2NzQyYTUyYzA0Nzg1ZjE2MDI1MDVlNTRiNmEiLCJpYXQiOjE3NTAxMTI5NzAuOTE5OTM0LCJuYmYiOjE3NTAxMTI5NzAuOTE5OTM1LCJleHAiOjQ5MDU3ODY1NzAuOTE0NjA4LCJzdWIiOiI3MjIyMjEwNiIsInNjb3BlcyI6WyJ1c2VyLnJlYWQiLCJ1c2VyLndyaXRlIiwidGFzay5yZWFkIiwidGFzay53cml0ZSIsIndlYmhvb2sucmVhZCIsIndlYmhvb2sud3JpdGUiLCJwcmVzZXQucmVhZCIsInByZXNldC53cml0ZSJdfQ.Bl91cayJ9svh1O7RPdgV7BeyUrDwBEE0JmwJeebZT-ecR1QPqHSlW6NiD5OOyJY4S7lthUZNLnKtxPRjsFyHuxV5Zi-pNQZakOEjK4Tm_tbGzr35Tk-crcNWOt48avn0hgCtI13s8U8oBbjLMzu4vW0vMMzZTKCEVa0vJl6DTsB5Vp14SnxxoX2HF5-XBLzT1uu76THXu3QrVxxNjAlSlXH2aH0_BSI0zgEFqzWy3iySLBYVqs9cY5d3tpoYbAGKX0CfGszSK-6YiAJPeTCA1Qm6kjM0I-nwB7XDH4tIbiHNgn4zGk4_yBowm53A3IwCAT-GX03Z25vl0hkBtvuXLPOSROsBXd1q-ds4Wm_-BMX1I411ABDkNxNKJGe9in6sw62R4maXPaGT-NNl7PXDDqfXfAtAuQkXWPQJiuj6onLaNXlks1o6dTQE-dbYfOufQy-4doiBSQL4vfaKfBpRdPCHslclgoogJ5n4e1XAsWgkqAiRHg9xYefqAfrgxTiqWWSV5XsjZQs7ylMK4zku9gQIj1esRdYiVzoXlynSR64due3k9mADVTD3zMYKzfvOgMBtJuOXzvz5mI4QaGVVLWKYpf0ecZ1DGE768WzOxgnki2XXYOLqHliMNJDa7PpW7SXIeYuOEDTAOO7UHHhT5hkCl2NkwJTgLX7KhqHjz04Y'))
+cloudconvert.configure(api_key=os.environ.get('CLOUDCONVERT_API_KEY'))
 
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 GITHUB_REPO = "Sebasm140111/solicitud-backend"
@@ -29,7 +29,7 @@ def generar_pdf():
         doc.render(contexto)
         doc_path = "documento.docx"
         doc.save(doc_path)
-
+        print("📤 Iniciando trabajo en CloudConvert...")
         job = cloudconvert.Job.create(payload={
             "tasks": {
                 "import-my-file": {"operation": "import/upload"},
@@ -52,9 +52,10 @@ def generar_pdf():
             requests.post(upload_url, data=upload_params, files={'file': f})
 
         job = cloudconvert.Job.wait(id=job['id'])
+        print("✅ CloudConvert Job completado")
         export_task = next((t for t in job['tasks'] if t['name'] == 'export-my-file'), None)
         file_url = export_task['result']['files'][0]['url']
-
+        
         response = requests.get(file_url)
         with open("documento.pdf", "wb") as f:
             f.write(response.content)
